5742552013ed2e4a3795c74fe9b61e44df600f09
S-STALWART-S <stalwart.programmer@gmail.com>
2021-12-18 22:08:57 +0330

    add chat manual

--- src/Variables/constants/requestConstants.js
+++ src/Variables/constants/requestConstants.js
@@ -3,6 +3,7 @@ import { RUNTIME_MODE } from "~/Variables/constants/others";
 const baseURL =
 	RUNTIME_MODE === "production"
 		? "https://teletalk-server.herokuapp.com"
-		: "http://localhost:8080";
+		: "https://teletalk-server.herokuapp.com";
+// : "http://localhost:8080"
 
 export { baseURL };
