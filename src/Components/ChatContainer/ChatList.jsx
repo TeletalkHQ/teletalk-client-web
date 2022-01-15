a4e13907be0b313b06f9e090239c0dd07b5a58bc
S-STALWART-S <stalwart.programmer@gmail.com>
2022-01-15 22:51:07 +0330

    ...

--- src/Components/ChatContainer/ChatList.jsx
+++ src/Components/ChatContainer/ChatList.jsx
@@ -8,9 +8,36 @@ const ChatList = ({ selectedChat, chats = [] }) => {
 			<List
 				sx={{ width: "80%", overflowY: "scroll", height: "100%", scrollBehavior: "smooth" }}
 			>
-				{chats?.map((item, index) => {
-					return <ChatListItem key={index} selected={selectedChat === item?.privateID} />;
-				})}
+				{(() => {
+					try {
+						const chatList = chats?.map((chat, index) => {
+							// const findParticipant = item.participants.find(
+							// 	(participant) => participant.participantID === );
+
+							const messages = chat.messages;
+
+							const lastMessage = messages[messages.length - 1];
+
+							const senderName = lastMessage
+							
+							return (
+								<ChatListItem
+									key={index}
+									message={lastMessage.messageText}
+									name={}
+									selected={selectedChat.chatID === chat?.chatID}
+								/>
+							);
+						});
+
+						console.log(chatList);
+
+						return chatList;
+					} catch (error) {
+						console.log("ChatList", error);
+						return null;
+					}
+				})()}
 			</List>
 		</>
 	);
