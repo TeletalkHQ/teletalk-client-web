4f05459d5a71c5648e62c77281fcb543e5bd14bc
S-STALWART-S <stalwart.programmer@gmail.com>
2022-02-16 13:08:04 +0330

    ...

--- src/Reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { otherReducer } from "~/Reducers/otherReducer";
-import { userReducer } from "~/Reducers/userReducer";
-import { globalReducer } from "~/Reducers/globalReducer";
-import { rootReducer } from "~/Reducers/rootReducer";
-import { notificationReducer } from "~/Reducers/notificationReducer";
-
-export { otherReducer, userReducer, rootReducer, globalReducer, notificationReducer };
--- src/Variables/Constants/Others/otherConstants.js
+++
@@ -1,8 +0,0 @@
-const RUNTIME_MODE = process.env.NODE_ENV;
-
-const baseURL =
-	RUNTIME_MODE === "production"
-		? "https://teletalk-server.herokuapp.com"
-		: "http://localhost:8080";
-
-export { RUNTIME_MODE, baseURL };
