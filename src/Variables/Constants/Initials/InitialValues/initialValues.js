b2a1b4978b299d5c1889ee36f0a4fee4b7cb02e4
S-STALWART-S <stalwart.programmer@gmail.com>
2022-03-06 00:01:06 +0330

    some dir changes

--- src/Variables/Constants/Initials/InitialValues/initialValues.js
+++ src/Variables/Constants/Initials/InitialValues/initialValues.js
@@ -1,57 +1,72 @@
 import {
-	Brightness4Outlined,
-	CallOutlined,
-	CampaignOutlined,
-	PeopleOutline,
-	PermIdentity,
-	SettingsOutlined,
-	Menu as MenuIcon,
-	Search as SearchIcon,
-	ForumOutlined,
-	PersonOutlineOutlined,
-	SmartToyOutlined,
-	AnnouncementOutlined,
-	SettingsInputComponentOutlined,
-	LogoutOutlined,
+  Brightness4Outlined,
+  CallOutlined,
+  CampaignOutlined,
+  PeopleOutline,
+  PermIdentity,
+  SettingsOutlined,
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  ForumOutlined,
+  PersonOutlineOutlined,
+  SmartToyOutlined,
+  AnnouncementOutlined,
+  SettingsInputComponentOutlined,
+  LogoutOutlined,
 } from "@mui/icons-material";
 
 const fn = (text, target, Icon) => ({ text, target, Icon });
 
 const INITIAL_VIEW_MODE = {
-	SIGN_IN: "SIGN_IN",
-	VERIFY_SIGN_IN: "VERIFY_SIGN_IN",
-	NEW_USER_PROFILE: "NEW_USER_PROFILE",
-	MESSENGER: "MESSENGER",
+  SIGN_IN: "SIGN_IN",
+  VERIFY_SIGN_IN: "VERIFY_SIGN_IN",
+  NEW_USER_PROFILE: "NEW_USER_PROFILE",
+  MESSENGER: "MESSENGER",
 };
 
 //REDESIGN
 const initialValues = {
-	allChats: fn("All Chats", "allChats", ForumOutlined),
-	bot: fn("Bot", "bot", SmartToyOutlined),
-	calls: fn("Calls", "calls", PermIdentity),
-	channels: fn("Channels", "channels", CallOutlined),
-	contacts: fn("Contacts", "contacts", PeopleOutline),
-	editChats: fn("Edit Chats", "editChats", SettingsInputComponentOutlined),
-	groups: fn("Groups", "groups", CampaignOutlined),
-	logout: fn("Logout", "logout", LogoutOutlined),
-	menu: fn("Menu", "menu", MenuIcon),
-	newChannel: fn("New Channel", "newChannel", CallOutlined),
-	newGroup: fn("New Group", "newGroup", CampaignOutlined),
-	nightMode: fn("Night Mode", "nightMode", Brightness4Outlined),
-	personal: fn("Personal", "personal", PersonOutlineOutlined),
-	search: fn("Search", "search", SearchIcon),
-	settings: fn("Settings", "settings", SettingsOutlined),
-	unread: fn("Unread Messages", "unread", AnnouncementOutlined),
+  allChats: fn("All Chats", "allChats", ForumOutlined),
+  bot: fn("Bot", "bot", SmartToyOutlined),
+  calls: fn("Calls", "calls", PermIdentity),
+  channels: fn("Channels", "channels", CallOutlined),
+  contacts: fn("Contacts", "contacts", PeopleOutline),
+  editChats: fn("Edit Chats", "editChats", SettingsInputComponentOutlined),
+  groups: fn("Groups", "groups", CampaignOutlined),
+  logout: fn("Logout", "logout", LogoutOutlined),
+  menu: fn("Menu", "menu", MenuIcon),
+  newChannel: fn("New Channel", "newChannel", CallOutlined),
+  newGroup: fn("New Group", "newGroup", CampaignOutlined),
+  nightMode: fn("Night Mode", "nightMode", Brightness4Outlined),
+  personal: fn("Personal", "personal", PersonOutlineOutlined),
+  search: fn("Search", "search", SearchIcon),
+  settings: fn("Settings", "settings", SettingsOutlined),
+  unread: fn("Unread Messages", "unread", AnnouncementOutlined),
 };
 
-const initialContact = { firstName: "", lastName: "", phoneNumber: "", privateID: "" };
+const initialContact = {
+  firstName: "",
+  lastName: "",
+  phoneNumber: "",
+  privateID: "",
+};
 
 const initialMessage = {
-	text: "",
-	messageID: "",
-	senderID: "",
+  text: "",
+  messageID: "",
+  senderID: "",
 };
 
-const initialCountry = { countryCode: "", countryName: "", countryShortName: "" };
+const initialCountry = {
+  countryCode: "",
+  countryName: "",
+  countryShortName: "",
+};
 
-export { initialContact, initialCountry, initialMessage, initialValues, INITIAL_VIEW_MODE };
+export {
+  initialContact,
+  initialCountry,
+  initialMessage,
+  initialValues,
+  INITIAL_VIEW_MODE,
+};
