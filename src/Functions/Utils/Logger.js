69278843556e374cb6029727626acabe4dcdd72e
S-STALWART-S <stalwart.programmer@gmail.com>
2022-03-06 21:43:24 +0330

    ....

--- src/Functions/Utils/Logger.js
+++ src/Functions/Utils/Logger.js
@@ -1,123 +1,121 @@
 let _level;
 
 class Logger {
-	static levels = ["error", "warn", "info", "debug"];
+  static levels = ["error", "warn", "info", "debug"];
 
-	constructor(level) {
-		if (!_level) {
-			_level = level || "debug";
-			this.setLevel(level);
-		}
+  constructor(level) {
+    if (!_level) {
+      this.setLevel(level || "debug");
+    }
 
-		this.isBrowser = this.browserDetector();
+    this.isBrowser = this.browserDetector();
 
-		if (!this.isBrowser) {
-			this.colors = {
-				start: "\x1b[2m",
-				warn: "\x1b[35m",
-				info: "\x1b[33m",
-				debug: "\x1b[36m",
-				error: "\x1b[31m",
-				end: "\x1b[0m",
-			};
-		} else {
-			this.colors = {
-				start: "%c",
-				warn: "color : #ff00ff",
-				info: "color : #ffff00",
-				debug: "color : #00ffff",
-				error: "color : #ff0000",
-				end: "",
-			};
-		}
-		this.messageFormat = "[%t] [%l] - [%m]";
-	}
+    if (!this.isBrowser) {
+      this.colors = {
+        start: "\x1b[2m",
+        warn: "\x1b[35m",
+        info: "\x1b[33m",
+        debug: "\x1b[36m",
+        error: "\x1b[31m",
+        end: "\x1b[0m",
+      };
+    } else {
+      this.colors = {
+        start: "%c",
+        warn: "color : #ff00ff",
+        info: "color : #ffff00",
+        debug: "color : #00ffff",
+        error: "color : #ff0000",
+        end: "",
+      };
+    }
+    this.messageFormat = "[%t] [%l] - [%m]";
+  }
 
-	browserDetector() {
-		return (
-			typeof process === "undefined" ||
-			process.type === "renderer" ||
-			process.browser === true ||
-			process.__nwjs
-		);
-	}
+  browserDetector() {
+    return (
+      typeof process === "undefined" ||
+      process.type === "renderer" ||
+      process.browser === true ||
+      process.__nwjs
+    );
+  }
 
-	setLevel(level) {
-		_level = level;
-		if (this.isBrowser || this.browserDetector()) {
-			localStorage?.setItem("logLevel", level);
-		}
-	}
+  setLevel(level) {
+    _level = level;
+    if (this.isBrowser || this.browserDetector()) {
+      localStorage?.setItem("logLevel", level);
+    }
+  }
 
-	removeLevel() {
-		_level = undefined;
-		localStorage?.removeItem("logLevel");
-	}
+  removeLevel() {
+    this.setLevel(undefined);
+  }
 
-	/**
-	 * @param level {string}
-	 * @returns {boolean}
-	 */
-	canSend(level) {
-		return Logger.levels.indexOf(_level) >= Logger.levels.indexOf(level);
-	}
+  /**
+   * @param level {string}
+   * @returns {boolean}
+   */
+  canSend(level) {
+    return Logger.levels.indexOf(_level) >= Logger.levels.indexOf(level);
+  }
 
-	/**
-	 * @param message {string}
-	 */
-	warn(message) {
-		// todo remove later
-		if (_level === "debug") {
-			console.error(new Error().stack);
-		}
-		this._log("warn", message, this.colors.warn);
-	}
+  /**
+   * @param message {string}
+   */
+  warn(message) {
+    // todo remove later
+    if (_level === "debug") {
+      console.error(new Error().stack);
+    }
+    this._log("warn", message, this.colors.warn);
+  }
 
-	/**
-	 * @param message {string}
-	 */
-	info(message) {
-		this._log("info", message, this.colors.info);
-	}
+  /**
+   * @param message {string}
+   */
+  info(message) {
+    this._log("info", message, this.colors.info);
+  }
 
-	/**
-	 * @param message {string}
-	 */
-	debug(message) {
-		this._log("debug", message, this.colors.debug);
-	}
+  /**
+   * @param message {string}
+   */
+  debug(message) {
+    this._log("debug", message, this.colors.debug);
+  }
 
-	/**
-	 * @param message {string}
-	 */
-	error(message) {
-		this._log("error", message, this.colors.error);
-	}
+  /**
+   * @param message {string}
+   */
+  error(message) {
+    this._log("error", message, this.colors.error);
+  }
 
-	format(message, level) {
-		return this.messageFormat
-			.replace("%t", new Date().toISOString())
-			.replace("%l", level.toUpperCase())
-			.replace("%m", message);
-	}
+  format(message, level) {
+    return this.messageFormat
+      .replace("%t", new Date().toISOString())
+      .replace("%l", level.toUpperCase())
+      .replace("%m", message);
+  }
 
-	/**
-	 * @param level {string}
-	 * @param message {string}
-	 * @param color {string}
-	 */
-	_log(level, message, color) {
-		if (!_level) {
-			return;
-		}
-		if (this.canSend(level)) {
-			if (this.isBrowser) {
-				console.log(this.colors.start + this.format(message, level), color);
-			} else {
-				console.log(color + this.format(message, level) + this.colors.end);
-			}
-		}
-	}
+  /**
+   * @param level {string}
+   * @param message {string}
+   * @param color {string}
+   */
+  _log(level, message, color) {
+    if (!_level) {
+      return;
+    }
+    if (this.canSend(level)) {
+      if (this.isBrowser) {
+        console.log(this.colors.start + this.format(message, level), color);
+      } else {
+        console.log(color + this.format(message, level) + this.colors.end);
+      }
+    }
+  }
 }
 
 export { Logger };
