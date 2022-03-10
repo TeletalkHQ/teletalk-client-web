0a34be88a70b5af20d45894eabbdf2e961453ac2
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-03-10 14:03:03 +0330

    INITIAL_VIEW_MODE fixed

--- src/Functions/Utils/responseHandler.js
+++ src/Functions/Utils/responseHandler.js
@@ -8,78 +8,78 @@ import { userAction } from "~/Actions/UserActions/userActions";
 import { INITIAL_VIEW_MODE } from "~/Variables/Constants/Initials/InitialValues/initialValues";
 
 const responseHandler = (response) => {
-	try {
-		const rejection = () => {
-			throw response;
-		};
+  try {
+    const rejection = () => {
+      throw response;
+    };
 
-		const statusCode = response.statusCode || response.status;
+    const statusCode = response.statusCode || response.status;
 
-		if (statusCode >= 400 && statusCode < 500) {
-			switch (statusCode) {
-				case 400:
-					appDispatch({ type: "request result", payload: "BAD REQUEST!" });
-					rejection();
-					break;
+    if (statusCode >= 400 && statusCode < 500) {
+      switch (statusCode) {
+        case 400:
+          appDispatch({ type: "request result", payload: "BAD REQUEST!" });
+          rejection();
+          break;
 
-				case 401:
-					PersistentStorage.clear();
-					appDispatch(userAction({ ...userInitializer() }));
-					appDispatch(
-						viewModeAction({
-							viewMode: INITIAL_VIEW_MODE.signIn,
-						}),
-					);
-					rejection();
-					break;
+        case 401:
+          PersistentStorage.clear();
+          appDispatch(userAction({ ...userInitializer() }));
+          appDispatch(
+            viewModeAction({
+              viewMode: INITIAL_VIEW_MODE.SIGN_IN,
+            })
+          );
+          rejection();
+          break;
 
-				case 805:
-					appDispatch();
-					rejection();
-					break;
+        case 805:
+          appDispatch();
+          rejection();
+          break;
 
-				case 804:
-					appDispatch();
-					rejection();
-					break;
+        case 804:
+          appDispatch();
+          rejection();
+          break;
 
-				case 823:
-					appDispatch();
-					rejection();
-					break;
+        case 823:
+          appDispatch();
+          rejection();
+          break;
 
-				case 826:
-					appDispatch();
-					rejection();
-					break;
+        case 826:
+          appDispatch();
+          rejection();
+          break;
 
-				default:
-					appDispatch();
-					rejection();
-					break;
-			}
-		} else if (statusCode >= 500 && statusCode < 600) {
-			switch (statusCode) {
-				case 500:
-					appDispatch({ type: "reject", payload: response });
-					rejection();
-					break;
+        default:
+          appDispatch();
+          rejection();
+          break;
+      }
+    } else if (statusCode >= 500 && statusCode < 600) {
+      switch (statusCode) {
+        case 500:
+          appDispatch({ type: "reject", payload: response });
+          rejection();
+          break;
 
-				default:
-					appDispatch();
-					rejection();
-					break;
-			}
-		} else if (statusCode >= 600) {
-			//...
-			rejection();
-		}
+        default:
+          appDispatch();
+          rejection();
+          break;
+      }
+    } else if (statusCode >= 600) {
+      //...
+      rejection();
+    }
 
-		return response;
-	} catch (error) {
-		console.log("responseHandler catch", error);
-		throw error;
-	}
+    return response;
+  } catch (error) {
+    console.log("responseHandler catch", error);
+    throw error;
+  }
 };
 
 export { responseHandler };
