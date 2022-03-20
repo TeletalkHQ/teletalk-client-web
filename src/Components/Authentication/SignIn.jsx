3ddccabe4d2edc9a7282ba9dbcf148287312e9ec
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-03-20 19:02:19 +0330

    ...

--- src/Components/Authentication/SignIn.jsx
+++ src/Components/Authentication/SignIn.jsx
@@ -1,10 +1,10 @@
 import {
-	Box,
-	Avatar,
-	TextField,
-	Typography,
-	Container,
-	CircularProgress,
+  Box,
+  Avatar,
+  TextField,
+  Typography,
+  Container,
+  CircularProgress,
 } from "@mui/material";
 import { LockOutlined } from "@mui/icons-material";
 import { LoadingButton } from "@mui/lab";
@@ -12,107 +12,111 @@ import { LoadingButton } from "@mui/lab";
 import CountrySelector from "~/Components/Others/CountrySelector";
 
 const SignIn = ({
-	countries,
-	countryCode,
-	countryName,
-	countryNameInputValue,
-	loading,
-	onCountryCodeChange,
-	onPhoneNumberChange,
-	onCountryNameOnchange,
-	onCountryNameOnInputChange,
-	onSignInClick,
-	phoneNumber,
-	selectedCountry,
+  countries,
+  countryCode,
+  countryName,
+  countryNameInputValue,
+  loading,
+  onCountryCodeChange,
+  onPhoneNumberChange,
+  onCountryNameOnchange,
+  onCountryNameOnInputChange,
+  onSignInClick,
+  phoneNumber,
+  selectedCountry,
 }) => {
-	return (
-		<Container maxWidth="xl">
-			<Box
-				sx={{
-					marginTop: 8,
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}
-			>
-				<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-					<LockOutlined />
-				</Avatar>
-				<Typography component="h1" variant="h5">
-					Sign in
-				</Typography>
-				<Container maxWidth="xs">
-					<Box sx={{ mt: 1 }}>
-						<Typography component="p" variant="p" color="GrayText">
-							Please verify your country code and enter your mobile phone number.
-						</Typography>
-						<CountrySelector
-							countries={countries}
-							countryName={countryName}
-							countryNameInputValue={countryNameInputValue}
-							onCountryNameOnchange={onCountryNameOnchange}
-							onCountryNameOnInputChange={onCountryNameOnInputChange}
-							selectedCountry={selectedCountry}
-						/>
+  return (
+    <Container maxWidth="xl">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Container maxWidth="xs">
+          <Box sx={{ mt: 1 }}>
+            <Typography component="p" variant="p" color="GrayText">
+              Please verify your country code and enter your mobile phone
+              number.
+            </Typography>
 
-						<Box display="flex" justifyContent={"space-between"}>
-							<TextField
-								style={{ width: "90px" }}
-								margin="normal"
-								required
-								id="countryCode"
-								label="Code"
-								name="countryCode"
-								autoComplete="off"
-								InputProps={{
-									sx: { borderRadius: "10px" },
-									startAdornment: (
-										<>
-											<span>+</span>
-										</>
-									),
-								}}
-								value={countryCode}
-								onChange={onCountryCodeChange}
-							/>
-							<TextField
-								margin="normal"
-								required
-								fullWidth
-								id="phoneNumber"
-								label="Phone number"
-								name="phoneNumber"
-								autoComplete="tel-national"
-								style={{ marginLeft: "5px" }}
-								InputProps={{
-									sx: { borderRadius: "10px" },
-								}}
-								value={phoneNumber}
-								onChange={onPhoneNumberChange}
-							/>
-						</Box>
+            {countries && (
+              <CountrySelector
+                countries={countries}
+                countryName={countryName}
+                countryNameInputValue={countryNameInputValue}
+                onCountryNameOnchange={onCountryNameOnchange}
+                onCountryNameOnInputChange={onCountryNameOnInputChange}
+                selectedCountry={selectedCountry}
+              />
+            )}
 
-						<LoadingButton
-							fullWidth
-							loading={loading}
-							loadingIndicator={
-								<>
-									<span>Please wait...</span> &nbsp;&nbsp;
-									<CircularProgress size={25} color="info" />
-								</>
-							}
-							onClick={onSignInClick}
-							size="large"
-							sx={{ mt: 3, mb: 2, borderRadius: "10px" }}
-							variant="contained"
-						>
-							Next
-						</LoadingButton>
-					</Box>
-				</Container>
-			</Box>
-		</Container>
-	);
+            <Box display="flex" justifyContent={"space-between"}>
+              <TextField
+                style={{ width: "90px" }}
+                margin="normal"
+                required
+                id="countryCode"
+                label="Code"
+                name="countryCode"
+                autoComplete="off"
+                InputProps={{
+                  sx: { borderRadius: "10px" },
+                  startAdornment: (
+                    <>
+                      <span>+</span>
+                    </>
+                  ),
+                }}
+                value={countryCode}
+                onChange={onCountryCodeChange}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="phoneNumber"
+                label="Phone number"
+                name="phoneNumber"
+                autoComplete="tel-national"
+                style={{ marginLeft: "5px" }}
+                InputProps={{
+                  sx: { borderRadius: "10px" },
+                }}
+                value={phoneNumber}
+                onChange={onPhoneNumberChange}
+              />
+            </Box>
+
+            <LoadingButton
+              fullWidth
+              loading={loading}
+              loadingIndicator={
+                <>
+                  <span>Please wait...</span> &nbsp;&nbsp;
+                  <CircularProgress size={25} color="info" />
+                </>
+              }
+              onClick={onSignInClick}
+              size="large"
+              sx={{ mt: 3, mb: 2, borderRadius: "10px" }}
+              variant="contained"
+            >
+              Next
+            </LoadingButton>
+          </Box>
+        </Container>
+      </Box>
+    </Container>
+  );
 };
 
 export default SignIn;
