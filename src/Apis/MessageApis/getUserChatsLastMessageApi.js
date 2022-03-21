f6d11168c4779fb92bfe5976d284f33753928859
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-03-21 22:31:12 +0330

    getUserChatsLastMessageApi fixed.

--- src/Apis/MessageApis/getUserChatsLastMessageApi.js
+++ src/Apis/MessageApis/getUserChatsLastMessageApi.js
@@ -1,13 +1,25 @@
 import { requester } from "~/Functions/Utils/requester";
+import { StuffStore } from "~/Functions/Utils/StuffStore";
 
 const getUserChatsLastMessageApi = async (data) => {
   try {
+    const {
+      templates: {
+        routerTemplates: {
+          privateChatRouterTemplate: {
+            baseUrl,
+            chatsLastMessage: { route, method },
+          },
+        },
+      },
+    } = StuffStore;
+
     const response = await requester({
-      //TODO requester things
       data,
-      method: "POST",
-      url: "chat/private/chats/last/message",
+      method,
+      url: `${baseUrl}${route}`,
     });
+
     return response;
   } catch (error) {
     console.log("getUserChatsLastMessageApi catch", error);
