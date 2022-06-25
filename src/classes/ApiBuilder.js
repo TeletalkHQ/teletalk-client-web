cb0cd2b3a5b4b68828ed852447c16e5edf537467
S-STALWART-S <stalwart.programmer@gmail.com>
2022-06-26 00:30:29 +0430

    added: [ i/o fields check to api builder (not tested) ]

--- src/classes/ApiBuilder.js
+++ src/classes/ApiBuilder.js
@@ -1,4 +1,9 @@
 import { requester } from "functions/utilities/requester";
+import { persistentStorage } from "classes/PersistentStorage";
+import {
+  checkInputFields,
+  checkOutputFields,
+} from "functions/helpers/inputOutputFieldsChecker";
 
 class ApiBuilder {
   constructor() {
@@ -36,14 +41,26 @@ class ApiBuilder {
     this.routeObject = routeObject;
     return this;
   }
+  setData(data) {
+    this.data = data;
+    return this;
+  }
+  async sendRequest({
+    token = persistentStorage.getItem({ key: "mainToken" }),
+    ...data
+  } = {}) {
+    console.log(this.routeObject);
 
-  async sendRequest({ token, ...data } = {}) {
+    checkInputFields(data, this.routeObject.inputFields);
+
     try {
       const response = await requester({
         data: data,
         ...this.getApiUrlAndMethod(this.baseUrlObject, this.routeObject),
         token,
       });
+
+      checkOutputFields(response.data, this.routeObject.outputFields);
 
       return response;
     } catch (error) {
