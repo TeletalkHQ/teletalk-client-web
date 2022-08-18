c69eaeae0a08bb6a949bf05c1d852488940c6c19
S-STALWART-S <stalwart.programmer@gmail.com>
2022-08-18 14:50:03 +0430

    etc commit

--- src/functions/otherFunctions/socket.js
+++ src/functions/otherFunctions/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 import { appConfigs } from "classes/AppConfigs";
 
-const socket = io(appConfigs.getConfigs().others.SERVER_BASE_URL);
+const socket = io(appConfigs.getConfigs().apiConfigs.SERVER_BASE_URL);
 
 export { socket };
