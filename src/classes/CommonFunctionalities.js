9dc1d7e1f7b868db3adc23f74fcfa0cdbd20fcc3
S-STALWART-S <stalwart.programmer@gmail.com>
2022-08-24 17:26:15 +0430

    added some useful fn to CommonFunctionalities

--- src/classes/CommonFunctionalities.js
+++ src/classes/CommonFunctionalities.js
@@ -3,6 +3,8 @@ import { tempActions } from "actions/tempActions";
 import { userActions } from "actions/userActions";
 
 import { commonNotificationManager } from "classes/CommonNotificationManager";
+import { notificationManager } from "classes/NotificationManager";
+import { objectUtilities } from "classes/ObjectUtilities";
 import { persistentStorage } from "classes/PersistentStorage";
 import { userPropsUtilities } from "classes/UserPropsUtilities";
 import { windowUtilities } from "classes/WindowUtilities";
@@ -12,6 +14,7 @@ import { checkErrorCodeIsConnAborted } from "functions/utilities/otherUtilities"
 import { extractedDispatch } from "hooks/useThunkReducer";
 
 import { VIEW_MODES } from "variables/otherVariables/constants";
+import { stuffStore } from "./StuffStore";
 
 class CommonFunctionalities {
   resetEverything() {
@@ -45,6 +48,7 @@ class CommonFunctionalities {
 
   changeViewMode() {
     const { MESSENGER, NEW_USER_PROFILE, SIGN_IN, VERIFY_SIGN_IN } = VIEW_MODES;
+
     const viewModeChanger = (viewMode) => () =>
       extractedDispatch(globalActions.viewModeChangeAction({ viewMode }));
 
@@ -54,6 +58,39 @@ class CommonFunctionalities {
       signIn: viewModeChanger(SIGN_IN),
       verifySignIn: viewModeChanger(VERIFY_SIGN_IN),
     };
+  }
+
+  correctServerLikeErrors(errors) {
+    const arrayOfErrors = objectUtilities.objectValues(errors);
+
+    const correctedErrors = arrayOfErrors.map((errorItem) => {
+      const { errorCode, reason, ...finalErrorItem } = errorItem;
+
+      finalErrorItem.notificationCode = errorCode;
+      finalErrorItem.notificationReason = reason;
+
+      return finalErrorItem;
+    });
+
+    return correctedErrors;
+  }
+
+  errorsPrinter(errors) {
+    errors.forEach((errorItem) => {
+      const { errorMessages } = stuffStore.languageData;
+      const errorUniqueId = errorItem.notificationReason;
+      const message = errorMessages[errorUniqueId];
+
+      const notificationObject = { ...errorItem, message };
+      console.log(notificationObject);
+      notificationManager.submitErrorNotification(notificationObject);
+    });
+  }
+
+  correctErrorsAndPrint(errors) {
+    const correctedErrors = this.correctServerLikeErrors(errors);
+
+    this.errorsPrinter(correctedErrors);
   }
 }
 
