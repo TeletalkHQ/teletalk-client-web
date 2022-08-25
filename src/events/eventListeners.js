210562fdff05fa2f959e76e16c0c5a0b0069202e
S-STALWART-S <stalwart.programmer@gmail.com>
2022-08-25 16:39:46 +0430

    etc commit

--- src/events/eventListeners.js
+++ src/events/eventListeners.js
@@ -1,11 +1,14 @@
 import { apiManager } from "classes/apiClasses/ApiManager";
+import { stuffStore } from "classes/StuffStore";
 import { validatorManager } from "classes/ValidatorManager";
 import { windowUtilities } from "classes/WindowUtilities";
 
 const thingsToDoAfterAllStuffReceived = () => {
+  const { version, ...validationModels } = stuffStore.validationModels;
+
   apiManager.rebuildAllApis();
   windowUtilities.addProperty("apiManager", apiManager);
-  validatorManager.compileValidators();
+  validatorManager.compileValidators(validationModels);
 };
 
 export { thingsToDoAfterAllStuffReceived };
