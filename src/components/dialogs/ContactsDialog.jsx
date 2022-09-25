5a9cd58389ed7fb807f81089f43413ff03e3238c
S-STALWART-S <stalwart.programmer@gmail.com>
2022-09-25 17:52:35 +0330

    etc commit

--- src/components/dialogs/ContactsDialog.jsx
+++ src/components/dialogs/ContactsDialog.jsx
@@ -6,18 +6,56 @@ import DialogTemplate from "components/dialogs/DialogTemplate";
 import CustomButton from "components/generals/inputs/CustomButton";
 import CustomBox from "components/generals/boxes/CustomBox";
 import CustomFlexBox from "components/generals/boxes/CustomFlexBox";
-import CustomTypography from "components/generals/typographies/CustomTypography";
 
 import { elementNames } from "variables/initials/initialValues/elementNames";
+import ContactListItem from "components/dialogs/ContactListItem";
+import H5 from "components/generals/typographies/H5";
 
+const ContactsTitle = () => (
+  <>
+    <CustomFlexBox jc="center" ai="center">
+      <H5>Contacts</H5>
+    </CustomFlexBox>
+  </>
+);
+
+const ContactsActions = ({ onClose, onAddContactClick }) => (
+  <>
+    <CustomFlexBox
+      sx={{ width: "100%" }}
+      jc="space-between"
+      gap={2}
+      ai="center"
+    >
+      <CustomBox>
+        <CustomButton
+          variant="text"
+          style={{ fontWeight: "bold" }}
+          onClick={onAddContactClick}
+        >
+          Add Contact
+        </CustomButton>
+      </CustomBox>
+      <CustomBox>
+        <CustomButton
+          variant="text"
+          style={{ fontWeight: "bold" }}
+          onClick={onClose}
+        >
+          Close
+        </CustomButton>
+      </CustomBox>
+    </CustomFlexBox>
+  </>
+);
 const ContactsDialog = ({ onDialogClose }) => {
   const {
     hooksOutput: { dispatch },
     state: {
       globalState: { dialogState },
+      userState,
     },
   } = useMainContext();
-
   const handleAddContactClick = () => {
     dispatch(
       globalActions.dialogOpenChangeAction({
@@ -31,45 +69,23 @@ const ContactsDialog = ({ onDialogClose }) => {
     onDialogClose("contacts");
   };
 
-  const titleContent = (
-    <>
-      <CustomFlexBox jc="space-between" ai="center">
-        <CustomBox>
-          <CustomTypography>Contacts</CustomTypography>
-        </CustomBox>
-        <CustomBox></CustomBox>
-      </CustomFlexBox>
-    </>
-  );
-
-  // const dialogContent = userState.contacts?.map((contact, index) => (
-  //   <ContactListItem
-  //     key={index}
-  //     name={`${contact.firstName} ${contact.lastName}`}
-  //     onContactClick={() => handleContactClick(contact)}
-  //   />
-  // ));
-
-  const actionContent = (
-    <>
-      <CustomFlexBox sx={{ width: "100%" }} jc="space-between" ai="center">
-        <CustomBox>
-          <CustomButton onClick={handleAddContactClick}>
-            Add Contact
-          </CustomButton>
-        </CustomBox>
-        <CustomBox>
-          <CustomButton onClick={handleClose}>Close</CustomButton>
-        </CustomBox>
-      </CustomFlexBox>
-    </>
-  );
+  const dialogContent = userState.contacts?.map((contact, index) => (
+    <ContactListItem
+      key={index}
+      name={`${contact.firstName} ${contact.lastName}`}
+    />
+  ));
 
   return (
     <DialogTemplate
-      titleContent={titleContent}
-      actionContent={actionContent}
-      // dialogContent={dialogContent}
+      titleContent={<ContactsTitle />}
+      actionContent={
+        <ContactsActions
+          onClose={handleClose}
+          onAddContactClick={handleAddContactClick}
+        />
+      }
+      dialogContent={dialogContent}
       open={dialogState.contacts.open}
       paperStyle={{ height: "90vh" }}
       onClose={handleClose}
