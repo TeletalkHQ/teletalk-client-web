e359bd3f1ea2c0b3e6f298586d45da58a0a86b79
s-stalwart-s <stalwart.programmer@gmail.com>
2022-11-17 08:44:03 +0330

    { 	"fix": { 		"versionControlApis": ["getAllStuff properties fixed"], 		"ApiManager": ["getAllPrivateChatsInfo renamed to getChatInfo"] 	} }

--- src/apis/versionControlApis.js
+++ src/apis/versionControlApis.js
@@ -1,26 +1,23 @@
 import { apiBuilder } from "classes/api/ApiBuilder";
 
-const { getAllStuffsRoute } = {
-  getAllStuffsRoute: {
-    description:
-      "Use for get all routes, models, validation models, errors and more",
-    fullUrl: "/versionControl/getAllStuff",
-    inputFields: [
-      {
-        language: "",
-      },
-    ],
-    method: "get",
-    optionalFields: [{}],
-    outputFields: [{}],
-    statusCode: 200,
-    url: "/getAllStuff",
-  },
+const getAllStuffs = {
+  description:
+    "Use for get all routes, models, validation models, errors and more",
+  fullUrl: "/versionControl/getAllStuff",
+  inputFields: [
+    {
+      language: { type: "string" },
+    },
+  ],
+  method: "post",
+  outputFields: [{}],
+  statusCode: 200,
+  url: "/getAllStuff",
 };
 
 const getAllStuffApi = apiBuilder
   .create()
-  .setRequirements({ routeObject: getAllStuffsRoute })
+  .setRequirements({ routeObject: getAllStuffs })
   .build();
 
 export { getAllStuffApi };
