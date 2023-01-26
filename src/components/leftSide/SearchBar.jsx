761488bf235efb85f165e06580dd2c8b49119177
s-stalwart-s <stalwart.programmer@gmail.com>
2023-01-26 15:28:11 +0330

    {   "fix": ["SearchBar"] }

--- src/components/leftSide/SearchBar.jsx
+++ src/components/leftSide/SearchBar.jsx
@@ -8,26 +8,29 @@ import { Icons } from "src/components/other/Icons";
 const SearchBar = ({ onDrawerIconClick }) => {
   return (
     <>
-      <Box.Div style={{ padding: 10 }}>
-        <IconButton onClick={onDrawerIconClick}>
-          <Icons.Menu.Icon />
-        </IconButton>
-      </Box.Div>
+      <Box.Flex style={{ width: "100%" }} jc="space-between" ai="center">
+        <Box.Div style={{ paddingLeft: 5 }}>
+          <IconButton onClick={onDrawerIconClick}>
+            <Icons.Menu.Icon />
+          </IconButton>
+        </Box.Div>
 
-      <Box.Div>
-        <Input.Text
-          size="small"
-          placeholder="Search"
-          InputProps={{
-            style: { borderRadius: "50px" },
-            startAdornment: (
-              <InputAdornment position="start">
-                <Icons.Search.Icon />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Box.Div>
+        <Box.Div style={{ width: "90%", padding: 5 }}>
+          <Input.Text
+            fullWidth
+            size="small"
+            placeholder="Search"
+            InputProps={{
+              style: { borderRadius: 50 },
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Icons.Search.Icon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box.Div>
+      </Box.Flex>
     </>
   );
 };
