bd8fd8ab588dc602cbd14ba1a6b7d5221bd6b097
s-stalwart-s <stalwart.programmer@gmail.com>
2023-01-31 15:31:54 +0330

    { 	"refactor": ["EditUsername dialog"] }

---
+++ src/components/dialog/editUsername/Content.jsx
@@ -0,0 +1,34 @@
+import { Box } from "src/components/general/box";
+import { Input } from "src/components/general/input";
+import InputAdornment from "src/components/general/other/InputAdornment";
+import GreyTextParagraph from "src/components/general/typography/GreyTextParagraph";
+
+const EditUsernameContent = ({
+  username,
+  usernameModelLength,
+  onInputChange,
+}) => {
+  return (
+    <Box.Flex style={{ maxWidth: 400 }} col>
+      <Input.Text
+        autoFocus
+        name="username"
+        label="Username"
+        onChange={onInputChange}
+        value={username}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">@</InputAdornment>,
+        }}
+      />
+
+      <GreyTextParagraph>
+        You can choose a username on Teletalk. If you do, other people will be
+        able to find you by this username and contact you without knowing your
+        phone number. You can use a-z, 0-9 and underscores. Minimum length is{" "}
+        {usernameModelLength} characters.
+      </GreyTextParagraph>
+    </Box.Flex>
+  );
+};
+
+export default EditUsernameContent;
