43b1c47e0ee01f2bf0efa13750b00223a3f71cfd
s-stalwart-s <stalwart.programmer@gmail.com>
2023-01-31 16:01:36 +0330

    { 	"refactor": ["EditFullName dialog"] }

---
+++ src/components/dialog/editFullName/index.js
@@ -0,0 +1,11 @@
+import Actions from "src/components/dialog/editFullName/Actions";
+import Content from "src/components/dialog/editFullName/Content";
+import Title from "src/components/dialog/editFullName/Title";
+
+const EditFullNameComponents = {
+  Actions,
+  Content,
+  Title,
+};
+
+export default EditFullNameComponents;
