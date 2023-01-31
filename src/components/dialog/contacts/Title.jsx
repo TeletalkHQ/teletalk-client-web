a6578da049ff73a3c4085b8316b0cbc37b017894
s-stalwart-s <stalwart.programmer@gmail.com>
2023-01-31 16:24:01 +0330

    { 	"refactor": ["Contacts dialog"] }

---
+++ src/components/dialog/contacts/Title.jsx
@@ -0,0 +1,12 @@
+import { Box } from "src/components/general/box";
+import H5 from "src/components/general/typography/header/H5";
+
+const ContactsTitle = () => (
+  <>
+    <Box.Flex jc="center" ai="center">
+      <H5>Contacts</H5>
+    </Box.Flex>
+  </>
+);
+
+export default ContactsTitle;
---
+++ src/components/dialog/contacts/index.js
@@ -0,0 +1,11 @@
+import Actions from "src/components/dialog/contacts/Actions";
+import Content from "src/components/dialog/contacts/Content";
+import Title from "src/components/dialog/contacts/Title";
+
+const ContactsComponents = {
+  Actions,
+  Content,
+  Title,
+};
+
+export default ContactsComponents;
