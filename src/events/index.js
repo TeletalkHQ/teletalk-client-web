be071a5dd168a871c7fbc4a1f7bc14ccff255d87
s-stalwart-s <stalwart.programmer@gmail.com>
2023-02-14 01:43:08 +0330

    register events/websocket/other

--- src/events/index.js
+++ src/events/index.js
@@ -1,7 +1,10 @@
 import { addOnlineStatusEvents } from "src/events/onlineConnectionsChecker";
 
+import { websocketEvents } from "src/events/websocket";
+
 const events = {
   addOnlineStatusEvents,
+  websocket: websocketEvents,
 };
 
 export { events };
