b8ca589321f6de3679dd7d6815e11c26f949612f
s-stalwart-s <stalwart.programmer@gmail.com>
2023-02-16 23:16:12 +0330

    add websocket EventBuilder

---
+++ src/classes/websocket/EventBuilder.js
@@ -0,0 +1,64 @@
+import lodash from "lodash";
+
+import { eventHandler } from "src/classes/websocket/EventHandler";
+
+import { utilities } from "src/utilities";
+
+import { variables } from "src/variables";
+
+class EventBuilder {
+  constructor() {
+    this.requirements = {
+      requestInterceptors: [],
+      requestTransformer: (data) => data,
+      responseInterceptors: [],
+      responseTransformer: (data) => data,
+      route: {},
+    };
+  }
+
+  setRequirements(requirements) {
+    Object.entries(requirements).forEach(([key, value]) => {
+      this[`set${lodash.upperFirst(key)}`](value);
+    });
+    return this;
+  }
+  setRoute(route) {
+    this.requirements.route = route;
+    return this;
+  }
+  setRequestInterceptors(...callbacks) {
+    this.requirements.requestInterceptors = callbacks;
+    return this;
+  }
+  setResponseInterceptors(...callbacks) {
+    this.requirements.responseInterceptors = callbacks;
+    return this;
+  }
+  setRequestTransformer(callback = this.requestTransformer) {
+    this.requirements.requestTransformer = callback;
+    return this;
+  }
+  setResponseTransformer(callback = this.responseTransformer) {
+    this.requirements.responseTransformer = callback;
+    return this;
+  }
+
+  checkMinimumRequirements() {
+    utilities.errorThrower(!this.requirements.route.name, {
+      ...variables.notification.error.EVENT_IS_BROKEN,
+      requirements: this.requirements,
+    });
+  }
+  build() {
+    this.checkMinimumRequirements();
+
+    return eventHandler.create(this.requirements);
+  }
+}
+
+const eventBuilder = {
+  create: () => new EventBuilder(),
+};
+
+export { eventBuilder, EventBuilder };
