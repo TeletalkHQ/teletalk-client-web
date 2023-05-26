ca173d5686bc944deaaa481ae08a06b1b928a117
s-stalwart-s <stalwart.programmer@gmail.com>
2023-05-27 01:29:25 +0330

    refactor: phoneNumber input to ts

--- src/components/general/input/common/PhoneNumber.tsx
+++ src/components/general/input/common/PhoneNumber.tsx
@@ -1,18 +1,26 @@
+import { ChangeEventHandler } from "react";
+
 import { Input } from "~/components/general/input";
+
 import withInputValidator from "~/hoc/withInputValidator";
 
 import { variables } from "~/variables";
 
-const PhoneNumberInput = ({ inputValue, onInputChange }) => {
+interface Props {
+  onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+  value: unknown;
+}
+
+const PhoneNumberInput: React.FC<Props> = ({ value, onChange }) => {
   return (
     <Input.Text
       autoComplete="tel-national"
       label={variables.other.helper.ELEMENT_LABELS.PHONE_NUMBER}
       name={variables.other.helper.ELEMENT_NAMES.PHONE_NUMBER}
-      onChange={onInputChange}
+      onChange={onChange}
       required
       style={{ marginLeft: "5px" }}
-      value={inputValue}
+      value={value}
     />
   );
 };
