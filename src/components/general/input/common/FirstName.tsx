3c6b28a2ac2857981cdd30d352341325ab1ab573
s-stalwart-s <stalwart.programmer@gmail.com>
2023-06-02 04:44:34 +0330

    refactor: FirstName to ts

--- src/components/general/input/common/FirstName.tsx
+++ src/components/general/input/common/FirstName.tsx
@@ -1,18 +1,30 @@
 import { Input } from "~/components/general/input";
 import withInputValidator from "~/hoc/withInputValidator";
 
+import { CommonOnChange } from "~/types";
+
 import { variables } from "~/variables";
 
-const FirstNameInput = ({ inputValue, onInputChange, required = true }) => {
+interface Props {
+  value: string;
+  onChange: CommonOnChange;
+  required?: boolean;
+}
+
+const FirstNameInput: React.FC<Props> = ({
+  value,
+  onChange,
+  required = true,
+}) => {
   return (
     <Input.Text
       autoFocus
       id={variables.other.helper.ELEMENT_IDS.FIRST_NAME_INPUT}
       label={variables.other.helper.ELEMENT_LABELS.FIRST_NAME}
-      name={variables.other.helper.ELEMENT_NAMES.FIRST_NAME}
-      onChange={onInputChange}
+      name={variables.other.helper.ELEMENT_NAMES.firstName}
+      onChange={onChange}
       required={required}
-      value={inputValue}
+      value={value}
     />
   );
 };
