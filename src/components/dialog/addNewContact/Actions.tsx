f4eb9f68cf8e805b18538f5fee45d576ce72b1e2
s-stalwart-s <stalwart.programmer@gmail.com>
2023-06-06 02:46:13 +0330

    refactor: AddNewContact dialog to ts

--- src/components/dialog/addNewContact/Actions.tsx
+++ src/components/dialog/addNewContact/Actions.tsx
@@ -1,40 +1,39 @@
-import { componentBuilder } from "~/classes/ComponentBuilder";
+import Box from "~/components/general/box";
 
-import { Box } from "~/components/general/box";
 import { Input } from "~/components/general/input";
 
-const AddNewContactActions = componentBuilder
-  .create()
-  .registerComponent(
-    "AddNewContactActions",
-    ({
-      onAddNewContactClick,
-      onContactDialogCancelClick,
-      isAddNewContactButtonDisabled,
-    }) => {
-      return (
-        <>
-          {/* //TODO: Extract to static vars */}
-          <Box.Flex gap={1} jc="flex-end" ai="center">
-            <Box.Div>
-              <Input.Button variant="text" onClick={onContactDialogCancelClick}>
-                Cancel
-              </Input.Button>
-            </Box.Div>
-            <Box.Div>
-              <Input.Button
-                disabled={isAddNewContactButtonDisabled}
-                variant="text"
-                onClick={onAddNewContactClick}
-              >
-                Create
-              </Input.Button>
-            </Box.Div>
-          </Box.Flex>
-        </>
-      );
-    }
-  )
-  .build();
+import { VoidNoArgsFn } from "~/types";
+
+interface Props {
+  isAddNewContactButtonDisabled: boolean;
+  onAddNewContactClick: VoidNoArgsFn;
+  onContactDialogCancelClick: VoidNoArgsFn;
+}
+const AddNewContactActions: React.FC<Props> = ({
+  isAddNewContactButtonDisabled,
+  onAddNewContactClick,
+  onContactDialogCancelClick,
+}) => {
+  return (
+    <>
+      <Box.Flex gap={1} jc="flex-end" ai="center">
+        <Box.Div>
+          <Input.Button variant="text" onClick={onContactDialogCancelClick}>
+            Cancel
+          </Input.Button>
+        </Box.Div>
+        <Box.Div>
+          <Input.Button
+            disabled={isAddNewContactButtonDisabled}
+            variant="text"
+            onClick={onAddNewContactClick}
+          >
+            Create
+          </Input.Button>
+        </Box.Div>
+      </Box.Flex>
+    </>
+  );
+};
 
 export default AddNewContactActions;
