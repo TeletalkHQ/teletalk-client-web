72fef5d52ecdcc6fa6f707a0ce0e503765f4ded4
s-stalwart-s <stalwart.programmer@gmail.com>
2023-06-06 02:46:13 +0330

    refactor: AddNewContact dialog to ts

--- src/components/dialog/addNewContact/Content.tsx
+++ src/components/dialog/addNewContact/Content.tsx
@@ -1,52 +1,54 @@
-import { componentBuilder } from "~/classes/ComponentBuilder";
-
-import { Box } from "~/components/general/box";
+import Box from "~/components/general/box";
 import { Input } from "~/components/general/input";
 
-const AddNewContactContent = componentBuilder
-  .create()
-  .registerComponent(
-    "AddNewContactContent",
-    ({
-      contact,
-      countries,
-      countryName,
-      onCountryCodeInputChange,
-      onCountryNameInputChange,
-      onInputChange,
-      onSelectedCountryChange,
-      selectedCountry,
-    }) => {
-      return (
-        <>
-          <Box.Div>
-            <Box.Div></Box.Div>
+import { CountryItem, CountryName, FullContact, VoidNoArgsFn } from "~/types";
 
-            <Box.Flex col jc="space-between" mt={2}>
-              <Input.FullName
-                firstName={contact.firstName}
-                lastName={contact.lastName}
-                onFirstNameInputChange={onInputChange}
-                onLastNameInputChange={onInputChange}
-              />
+interface Props {
+  contact: FullContact;
+  countryName: CountryName;
+  onCountryCodeInputChange: VoidNoArgsFn;
+  onCountryNameInputChange: VoidNoArgsFn;
+  onInputChange: VoidNoArgsFn;
+  onSelectedCountryChange: VoidNoArgsFn;
+  selectedCountry: CountryItem;
+}
 
-              <Input.Cellphone
-                countries={countries}
-                countryCode={contact.countryCode}
-                countryName={countryName}
-                onCountryCodeInputChange={onCountryCodeInputChange}
-                onCountryNameInputChange={onCountryNameInputChange}
-                onPhoneNumberInputChange={onInputChange}
-                onSelectedCountryChange={onSelectedCountryChange}
-                phoneNumber={contact.phoneNumber}
-                selectedCountry={selectedCountry}
-              />
-            </Box.Flex>
-          </Box.Div>
-        </>
-      );
-    }
-  )
-  .build();
+const AddNewContactContent: React.FC<Props> = ({
+  contact,
+  countryName,
+  onCountryCodeInputChange,
+  onCountryNameInputChange,
+  onInputChange,
+  onSelectedCountryChange,
+  selectedCountry,
+}) => {
+  return (
+    <>
+      <Box.Div>
+        <Box.Div></Box.Div>
+
+        <Box.Flex col jc="space-between" mt={2}>
+          <Input.FullName
+            firstName={contact.firstName}
+            lastName={contact.lastName}
+            onFirstNameInputChange={onInputChange}
+            onLastNameInputChange={onInputChange}
+          />
+
+          <Input.Cellphone
+            countryCode={contact.countryCode}
+            countryName={countryName}
+            onCountryCodeInputChange={onCountryCodeInputChange}
+            onCountryNameInputChange={onCountryNameInputChange}
+            onPhoneNumberInputChange={onInputChange}
+            onSelectedCountryChange={onSelectedCountryChange}
+            phoneNumber={contact.phoneNumber}
+            selectedCountry={selectedCountry}
+          />
+        </Box.Flex>
+      </Box.Div>
+    </>
+  );
+};
 
 export default AddNewContactContent;
