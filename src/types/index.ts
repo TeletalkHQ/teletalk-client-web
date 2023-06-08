b0601e2970926dda1aab6949a2db55f423b853ef
s-stalwart-s <stalwart.programmer@gmail.com>
2023-06-08 03:51:59 +0330

    refactor: dialog template

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { CSSProperties } from "react";
 
 import { FullName } from "utility-store/lib/types";
+import { Transitions } from "~/components/other/Transitions";
 
 import { countries } from "~/data/countries";
 
@@ -17,6 +18,16 @@ export interface Cellphone {
   countryCode: CountryCode;
   countryName: CountryName;
   phoneNumber: string;
+}
+
+export type AppDrawerAnchor = "botton" | "left" | "right" | "top";
+
+export type TransitionName = keyof typeof Transitions;
+
+export interface UiConfig {
+  appDrawerCurrentAnchor: AppDrawerAnchor;
+  dialogDefaultTransition: TransitionName;
+  maxNotification: number;
 }
 
 export type Style = CSSProperties;
