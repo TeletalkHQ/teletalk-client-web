a7623800ccb8e148c109f564a2026a49d825a8c7
s-stalwart-s <stalwart.programmer@gmail.com>
2023-06-11 20:47:16 +0330

    refactor: layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,20 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { CssBaseline } from "@mui/material";
-import { SnackbarProvider } from "notistack";
-import { ThemeProvider } from "@mui/material/styles";
-
-import { windowUtilities } from "~/classes/WindowUtilities";
-
-import { appConfigs } from "~/classes/AppConfigs";
-
-import { MainContext } from "~/context/MainContext";
-
 import { events } from "~/events";
-
-import { assignDispatch } from "~/helpers/extractedDispatch";
-
-import { baseTheme } from "~/theme/baseTheme";
 
 interface Props {
   children: JSX.Element;
@@ -26,32 +12,14 @@ const Layout: React.FC<Props> = ({ children }) => {
     const updater = () => {
       setForceUpdate(!forceUpdate);
     };
-    windowUtilities.addProperty("updater", updater);
+    window.updater = updater;
   }, [forceUpdate]);
-
-  useEffect(() => {
-    windowUtilities.addProperty("state", state);
-  }, [state]);
-
-  useEffect(() => {
-    assignDispatch(dispatch);
-  }, [dispatch]);
 
   useEffect(() => {
     events.websocket.otherEvents();
   }, []);
 
-  const maxNotification = appConfigs.getConfigs().ui.maxNotification;
-  return (
-    <SnackbarProvider maxSnack={maxNotification}>
-      <MainContext.Provider value={{}}>
-        <ThemeProvider theme={baseTheme}>
-          <CssBaseline enableColorScheme />
-          {children}
-        </ThemeProvider>
-      </MainContext.Provider>
-    </SnackbarProvider>
-  );
+  return <>{children}</>;
 };
 
 export default Layout;
