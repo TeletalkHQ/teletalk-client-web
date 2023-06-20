168c82b4eb88842e5658abd7d4b28eb121345946
s-stalwart-s <stalwart.programmer@gmail.com>
2023-06-20 21:33:18 +0330

    feat: remove notification store

--- src/store/notification/payloads.ts
+++
@@ -1,8 +0,0 @@
-import { fields } from "~/store/fields";
-import { NOTIFICATION_ACTION_TYPES } from "~/store/notification/types";
-
-const notificationActionPayloads = {
-  [NOTIFICATION_ACTION_TYPES.NEW_NOTIFICATION]: fields.collection.notification,
-};
-
-export { notificationActionPayloads };
