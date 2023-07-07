ace593a259ab45c09912bb714c6f44fadab7cad9
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-08 01:23:38 +0330

    feat: cleanup variables

--- src/variables/index.ts
+++ src/variables/index.ts
@@ -1,11 +1,9 @@
 import { common } from "~/variables/common";
-import { notification } from "~/variables/notification";
 import { other } from "~/variables/other";
 
 const variables = {
-  common,
-  notification,
-  other,
+  ...common,
+  ...other,
 };
 
 export { variables };
--- src/variables/other/index.ts
+++ src/variables/other/index.ts
@@ -1,5 +1,16 @@
+import { ConnectionColors } from "~/types";
 import { regex } from "~/variables/other/regex";
 
-const other = { regex };
+export const connectionColors: ConnectionColors = {
+  idle: "white",
+  offline: "red",
+  online: "green",
+  pending: "yellow",
+};
+
+const other = {
+  connectionColors,
+  regex,
+};
 
 export { other };
