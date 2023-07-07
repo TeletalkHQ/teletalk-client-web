db56351ff8336423c4ee46198cdc7b49e0b53f01
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-08 01:24:18 +0330

    feat: implement ServerSelect

---
+++ src/components/initialSetup/ServerSelect.tsx
@@ -0,0 +1,49 @@
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+
+import { appConfigs } from "~/classes/AppConfigs";
+
+import Box from "../general/box";
+import { Input } from "../general/input";
+
+interface Props {
+  onServerSelectChange: (url: string) => void;
+  selectedServer: string;
+}
+
+const ServerSelect: React.FC<Props> = ({
+  onServerSelectChange,
+  selectedServer,
+}) => {
+  return (
+    <>
+      <FormControl fullWidth>
+        <InputLabel id="labelId">Selected server</InputLabel>
+        <Input.Select
+          defaultValue={selectedServer}
+          labelId="labelId"
+          value={selectedServer}
+          label="Selected server"
+          onChange={(props) =>
+            onServerSelectChange(props.target.value as string)
+          }
+        >
+          {[
+            {
+              url: appConfigs.getConfigs().api.selectedServerUrl,
+            },
+            {
+              url: "hallo!",
+            },
+          ].map((item, index) => (
+            <Box.MenuItem key={index} value={item.url}>
+              {item.url}
+            </Box.MenuItem>
+          ))}
+        </Input.Select>
+      </FormControl>
+    </>
+  );
+};
+
+export default ServerSelect;
