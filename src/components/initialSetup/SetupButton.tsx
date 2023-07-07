d132027389b792ed730d74b1e474451a8855e709
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-08 01:24:42 +0330

    feat: implement initial setup button

---
+++ src/components/initialSetup/SetupButton.tsx
@@ -0,0 +1,32 @@
+import Box from "~/components/general/box";
+import { Input } from "~/components/general/input";
+import CircularProgress from "~/components/general/progress/CircularProgress";
+import { VoidNoArgsFn } from "~/types";
+
+interface Props {
+  loading: boolean;
+  onSetup: VoidNoArgsFn;
+  indicatorText: string;
+}
+
+const SetupButton: React.FC<Props> = ({ indicatorText, loading, onSetup }) => {
+  return (
+    <Input.LoadingButton
+      loading={loading}
+      onClick={onSetup}
+      style={{
+        textAlign: "center",
+      }}
+      loadingIndicator={
+        <Box.Span>
+          <Box.Span style={{ color: "white" }}>{indicatorText}</Box.Span>
+          <CircularProgress style={{ marginLeft: 10 }} size={20} color="info" />
+        </Box.Span>
+      }
+    >
+      Setup
+    </Input.LoadingButton>
+  );
+};
+
+export default SetupButton;
