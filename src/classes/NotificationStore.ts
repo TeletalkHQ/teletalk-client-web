028f28fcc6399d03deee6ccbfd61f70447029059
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-17 23:00:17 +0330

    feat: implement NotificationStore

---
+++ src/classes/NotificationStore.ts
@@ -0,0 +1,27 @@
+import { Notification, NotificationReason } from "~/types";
+import { customErrors } from "~/variables/notification";
+
+import { notificationBuilder } from "./NotificationBuilder";
+import { stuffStore } from "./StuffStore";
+
+class NotificationStore {
+  private notifications = [...stuffStore.errors, ...customErrors].map(
+    (item) => {
+      return notificationBuilder()
+        .reason(item.reason)
+        .isAuthError(item.isAuthError)
+        .side(item.side)
+        .build();
+    }
+  );
+
+  find(reason: NotificationReason): Notification {
+    return this.notifications.find((i) => i.reason === reason)!;
+  }
+
+  getAll() {
+    return this.notifications;
+  }
+}
+
+export const notificationStore = new NotificationStore();
