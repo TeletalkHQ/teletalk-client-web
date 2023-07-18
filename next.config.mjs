c7ca0261a8fd0094c5ea0190889d22dc35b09ddc
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-19 01:37:08 +0330

    feat: cleanup `nextJS` config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 
-dotenv.config({
-  path: "./configs/env/base.env",
-  override: true,
-});
-dotenv.config({
-  path: `./configs/env/${process.env.NODE_ENV}.env`,
-  override: true,
-});
+["./configs/env/base.env", `./configs/env/${process.env.NODE_ENV}.env`].forEach(
+  (path) => {
+    dotenv.config({
+      path,
+      override: true,
+    });
+  }
+);
 
 const env = Object.keys(process.env).reduce((prev, curr) => {
   if (curr.startsWith("NEXT_PUBLIC_")) prev[curr] = process.env[curr];
@@ -32,18 +32,13 @@ const nextConfig = {
   },
 
   async redirects() {
-    return [
-      { source: "/", destination: initialSetupRoute, permanent: true },
-      {
-        source: "/auth",
+    return ["/", "/auth", "/create", "/messenger", "/signIn", "/verify"].map(
+      (item) => ({
+        source: item,
         destination: initialSetupRoute,
         permanent: true,
-      },
-      { source: "/create", destination: initialSetupRoute, permanent: true },
-      { source: "/messenger", destination: initialSetupRoute, permanent: true },
-      { source: "/signIn", destination: initialSetupRoute, permanent: true },
-      { source: "/verify", destination: initialSetupRoute, permanent: true },
-    ];
+      })
+    );
   },
 };
 
