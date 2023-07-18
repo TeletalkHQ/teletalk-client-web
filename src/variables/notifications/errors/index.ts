8de9dff633c1420a586911eb7a057ebdf714a553
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-18 13:51:34 +0330

    feat: refactor variables usage

---
+++ src/variables/notifications/errors/index.ts
@@ -0,0 +1,7 @@
+import { checkFieldErrors } from "./checkFieldErrors";
+import { customErrors } from "./customErrors";
+
+export const errors = {
+  checkFieldErrors,
+  customErrors,
+};
--- src/variables/regex/index.ts
+++ src/variables/regex/index.ts
@@ -1,5 +1,7 @@
 const enNumber = new RegExp("^[0-9]+$");
 
 export const regex = {
-  numbers: { en: enNumber },
+  numbers: {
+    en: enNumber,
+  },
 };
