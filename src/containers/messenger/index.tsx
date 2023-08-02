3dac22edc3e78904f53908f9e5023fc751636502
s-stalwart-s <stalwart.programmer@gmail.com>
2023-08-03 03:13:38 +0330

    feat: add `useListener` for `updatePublicUserData`

--- src/containers/messenger/index.tsx
+++ src/containers/messenger/index.tsx
@@ -1,9 +1,25 @@
+import { extractor } from "~/classes/Extractor";
 import Box from "~/components/general/box";
 import LeftSide from "~/containers/messenger/leftSide";
 import Portal from "~/containers/messenger/portal";
 import RightSide from "~/containers/messenger/rightSide";
+import { useListener } from "~/hooks/useListener";
+import { useUserStore } from "~/store";
+import { SocketResponse, UpdatePublicUserDataIO } from "~/types";
 
 const Messenger = () => {
+  const userStore = useUserStore();
+
+  useListener({
+    evName: "updatePublicUserData",
+    cb: (response: SocketResponse<UpdatePublicUserDataIO["output"]>) => {
+      userStore.setUserData({
+        ...extractor.userData({ ...userStore, blacklist: [] }),
+        ...extractor.publicUserData(response.data.publicUserData),
+      });
+    },
+  });
+
   return (
     <Box.Grid
       container
