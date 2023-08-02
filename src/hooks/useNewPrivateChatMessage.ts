fcab2432964bf068b0983c8230d871ebce82f7db
s-stalwart-s <stalwart.programmer@gmail.com>
2023-08-03 02:13:41 +0330

    feat: refactor `useNewPrivateChatMessage`

--- src/hooks/useNewPrivateChatMessage.ts
+++ src/hooks/useNewPrivateChatMessage.ts
@@ -1,37 +1,20 @@
-import { useEffect } from "react";
-
-import { websocket } from "~/classes/websocket/Websocket";
 import { useMessageStore } from "~/store";
-import { EventName, SendPrivateMessageIO, SocketResponse } from "~/types";
 
 import { useAddPrivateChat } from "./useAddPrivateChat";
+import { useListener } from "./useListener";
 
 export const useNewPrivateChatMessage = () => {
   const messageStore = useMessageStore();
   const { updater: privateChatUpdater } = useAddPrivateChat();
 
-  useEffect(() => {
-    websocket.client.on<EventName>("sendPrivateMessage", updater);
-
-    return turnOffUpdater;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [messageStore]);
-
-  const turnOffUpdater = () => {
-    websocket.client.off<EventName>("sendPrivateMessage", updater);
-  };
-
-  const updater = async (
-    response: SocketResponse<SendPrivateMessageIO["output"]>
-  ) => {
-    if (
-      messageStore.privateChats.some((i) => i.chatId === response.data.chatId)
-    )
-      messageStore.addMessage(response.data);
-    else privateChatUpdater(response.data.chatId);
-  };
-
-  return {
-    updater,
-  };
+  useListener({
+    evName: "sendPrivateMessage",
+    cb: async (response) => {
+      if (
+        messageStore.privateChats.some((i) => i.chatId === response.data.chatId)
+      )
+        messageStore.addMessage(response.data);
+      else privateChatUpdater(response.data.chatId);
+    },
+  });
 };
