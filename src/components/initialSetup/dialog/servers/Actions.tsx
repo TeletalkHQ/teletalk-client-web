59cbbe1ba5e3cd39c60fda21ad998a094840ecec
s-stalwart-s <stalwart.programmer@gmail.com>
2023-08-03 21:41:25 +0330

    feat: refactor `Input.LoadingButton` usage

--- src/components/initialSetup/dialog/servers/Actions.tsx
+++ src/components/initialSetup/dialog/servers/Actions.tsx
@@ -1,6 +1,5 @@
 import { Input } from "~/components";
 import Box from "~/components/general/box";
-import CircularProgress from "~/components/general/progress/CircularProgress";
 import { VoidNoArgsFn } from "~/types";
 
 interface Props {
@@ -17,29 +16,7 @@ const Actions: React.FC<Props> = ({ loading, onPingAllClick }) => {
       }}
     >
       <Input.LoadingButton
-        loadingIndicator={
-          <>
-            <Box.Div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                gap: "10px",
-              }}
-            >
-              <Box.Span
-                style={{
-                  color: "white",
-                }}
-              >
-                Pinging...
-              </Box.Span>
-              <Box.Span>
-                <CircularProgress size={"18px"} />
-              </Box.Span>
-            </Box.Div>
-          </>
-        }
+        loadingIndicatorText="Pinging..."
         loading={loading}
         onClick={onPingAllClick}
       >
