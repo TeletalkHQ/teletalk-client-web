eeb009ec4282b44031641c7b9591fb0324a05b19
s-stalwart-s <stalwart.programmer@gmail.com>
2023-08-04 06:45:23 +0330

    fix: `AddContactWithCellphone` action buttons `width`

--- src/components/messenger/dialog/addContactWithCellphone/Actions.tsx
+++ src/components/messenger/dialog/addContactWithCellphone/Actions.tsx
@@ -16,22 +16,26 @@ const AddContactActions: React.FC<Props> = ({
 }) => {
   return (
     <>
-      <Box.Flex gap={1} jc="flex-end" ai="center">
-        <Box.Div>
-          <Input.Button variant="text" onClick={onContactDialogCancelClick}>
-            Cancel
-          </Input.Button>
-        </Box.Div>
-        <Box.Div>
-          <Input.LoadingButton
-            loadingIndicatorText="Updating..."
-            disabled={isAddContactButtonDisabled}
-            loading={loading}
-            onClick={onAddContactClick}
-          >
-            Create
-          </Input.LoadingButton>
-        </Box.Div>
+      <Box.Flex
+        style={{
+          width: "100%",
+        }}
+        gap={1}
+        jc="flex-end"
+        ai="center"
+      >
+        <Input.Button variant="text" onClick={onContactDialogCancelClick}>
+          Cancel
+        </Input.Button>
+
+        <Input.LoadingButton
+          loadingIndicatorText="Updating..."
+          disabled={isAddContactButtonDisabled}
+          loading={loading}
+          onClick={onAddContactClick}
+        >
+          Create
+        </Input.LoadingButton>
       </Box.Flex>
     </>
   );
