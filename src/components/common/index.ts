fd2ec2fbd1bd0f92d307b8691bbf4dd6d3f8a951
s-stalwart-s <stalwart.programmer@gmail.com>
2023-08-05 21:12:27 +0330

    fix: indexing new buttons

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,7 @@
 import Bio from "~/components/common/Bio";
 import Cellphone from "~/components/common/Cellphone";
+import CloseButton from "~/components/common/CloseButton";
+import ConfirmButton from "~/components/common/ConfirmButton";
 import CountryCode from "~/components/common/CountryCode";
 import FirstName from "~/components/common/FirstName";
 import FullName from "~/components/common/FullName";
@@ -9,9 +11,14 @@ import Username from "~/components/common/Username";
 import VerificationCode from "~/components/common/VerificationCode";
 import CountrySelector from "~/components/common/countrySelector";
 
+import CancelButton from "./CancelButton";
+
 export const CommonInput = {
   Bio,
+  CancelButton,
   Cellphone,
+  CloseButton,
+  ConfirmButton,
   CountryCode,
   CountrySelector,
   FirstName,
